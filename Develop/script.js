2316fd35ce5c4c98152d86d3a55a243e2474a30a
Rachel Wagschal <rawagschal@gmail.com>
2021-01-15 14:58:40 -0800

    input variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
 
+// input vars
+
+  var lowercase = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
+  var lowercaseArray = lowercase.split(',');
+
+  var uppercase = lowercase.toUpperCase();
+  var uppercaseArray = uppercase.split(',');
+  
+  var numbers = '1,2,3,4,5,6,7,8,9,0';
+  var numbersArray = numbers.split(',');
+
+  var specialChars = '!,@,#,$,%,^,&,*,(,),-,_,+,.,{,},~,`,?,/,|,=,<,>';
+  var specialCharsArray = specialChars.split(',')
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
